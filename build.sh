369ab88af8fe0d357ccba183bb1593328cf9cb24
Andreas Birch <andreasbirch1998@hotmail.com>
2024-01-18 09:19:22 +0100

    Updated file structure

--- build.sh
+++ build.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
 set -e
-
-# Build and install the libraries
-# abstracting away from using the
-# RabbitMq message queue
-pushd messaging-utilities-3.4
-./build.sh
-popd 
-
-# Build the services
-pushd logging-service
-./build.sh
-popd
+mvn clean package
+docker-compose build logging-service
